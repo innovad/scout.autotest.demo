43a185753926ba0699a9e56e317a9f41f5946702
adrian <trekking09@gmail.com>
2012-12-21 08:47:28 +0100

    First commit (All)

---
+++ org.eclipse.scout.autotest.server/src/org/eclipse/scout/autotest/server/services/outline/StandardOutlineService.java
@@ -0,0 +1,7 @@
+package org.eclipse.scout.autotest.server.services.outline;
+
+import org.eclipse.scout.autotest.shared.services.outline.IStandardOutlineService;
+import org.eclipse.scout.service.AbstractService;
+
+public class StandardOutlineService extends AbstractService implements IStandardOutlineService{
+}
