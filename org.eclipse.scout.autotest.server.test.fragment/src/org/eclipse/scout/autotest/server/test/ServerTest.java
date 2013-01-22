039b1fb48778f768eea2fc33120cea4af63f1040
adrian <trekking09@gmail.com>
2013-01-22 13:51:34 +0100

    Server Test: Use a Service

--- org.eclipse.scout.autotest.server.test.fragment/src/org/eclipse/scout/autotest/server/test/ServerTest.java
+++ org.eclipse.scout.autotest.server.test.fragment/src/org/eclipse/scout/autotest/server/test/ServerTest.java
@@ -2,7 +2,10 @@ package org.eclipse.scout.autotest.server.test;
 
 import org.eclipse.autotest.server.test.TychoSurfireServerTestRunner;
 import org.eclipse.scout.autotest.server.ServerSession;
+import org.eclipse.scout.autotest.shared.services.outline.IStandardOutlineService;
 import org.eclipse.scout.rt.testing.server.runner.ScoutServerTestRunner;
+import org.eclipse.scout.service.SERVICES;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -12,7 +15,9 @@ public final class ServerTest {
 
   @Test
   public void test() throws Exception {
-    // Dummy Test
+    // Dummy Server Test with Service
+    Object[][] data = SERVICES.getService(IStandardOutlineService.class).getCompanyTableData();
+    Assert.assertNotNull("Not Null", data);
   }
 
 }
